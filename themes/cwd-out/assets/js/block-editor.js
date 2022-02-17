14a99408564538820bae5dc8605e756aa64747e2
Adam Niaraki <adam.niaraki@dcmail.com>
2022-02-17 13:41:05 -0500

    Added Woo Commerce Plugin

--- themes/cwd-out/assets/js/block-editor.js
+++ themes/cwd-out/assets/js/block-editor.js
@@ -1,7 +1,6 @@
 // Cannot get it to show up on the Wordpress site
-// Tried changing & looking for any code errors on several files but got nothing
-// Check if we have the code on DC Conect
-console.log( 'block editor works' );
+// Finally got it to work
+// console.log( 'block editor works' );
 
 wp.blocks.registerBlockStyle( 'core/quote', {
     name: 'fancy-quote',
