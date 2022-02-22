766119be7563e07919c0112876ad02c5af784cce
Adam Niaraki <adam.niaraki@dcmail.com>
2022-02-22 15:07:12 -0500

    Added Book Review Plugin & Blog
    Added Book Review Plugin & my Blog Page

---
+++ plugins/book-review-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill'), 'version' => 'b55b4195238c9fc7c597c476d126f0e9');
