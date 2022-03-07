403289e06d2e0b3323ddc9449d46bf5e2d8dda04
Adam Niaraki <adam.niaraki@dcmail.com>
2022-03-07 00:26:11 -0500

    Header & Blog Update
    Attempted a few Dynamic Blog & Header updates but only a few stuck

--- themes/cwd-out/header.php
+++ themes/cwd-out/header.php
@@ -46,12 +46,15 @@
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cwd-out' ); ?></button>
 			<?php
+			// if ( has_nav_menu( 'menu-primary' ) ) {
 			wp_nav_menu(
 				array( 
 					'theme_location' => 'menu-primary',
 					'menu_id'        => 'primary-menu',
+					'container'		=> false,
 				)
 			);
+		// }
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
